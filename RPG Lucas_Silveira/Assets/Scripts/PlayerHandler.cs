a6561cd1913cac2c5159d008667c2d849ad20f8a
Professor <pedagogicomyrobotjoinville@gmail.com>
2025-09-13 11:08:03 -0300

    Ta show

--- RPG Lucas_Silveira/Assets/Scripts/PlayerHandler.cs
+++ RPG Lucas_Silveira/Assets/Scripts/PlayerHandler.cs
@@ -52,7 +52,7 @@ public class PlayerHandler : MonoBehaviour
     public void GainExperience(int experience)
     {
         playerExperience += experience;
-        if (playerExperience >= playerMaxExperience)
+        while (playerExperience >= playerMaxExperience)
         {
             LevelUp();
         }
